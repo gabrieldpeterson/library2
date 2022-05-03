b356bb812a2fb69c2e05feee5860a32bcfe31ffa
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-05-03 16:41:20 -0700

    Add sample books and add to array

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ function addBookToLibrary() {
   // function
 }
 
-const testBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const testBook1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+myLibrary.push(testBook1);
 
-testBook.info();
+const testBook2 = new Book('The Way of Kings', 'Brandon Sanderson', 1008, true);
+myLibrary.push(testBook2);
+
+const testBook3 = new Book('Words of Radiance', 'Brandon Sanderson', 1088, true);
+myLibrary.push(testBook3);
+
+const testBook4 = new Book('Hyperion', 'Dan Simmons', 480, false);
+myLibrary.push(testBook4);
+
+myLibrary.forEach((book) => {
+  book.info();
+});
